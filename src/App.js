615cb696b67bdfc2974ad1788bafd914fcfe170b
Pham Huu Hien <phhien203@gmail.com>
2022-05-23 22:10:21 +0700

    Navigate with the router's Link component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./temperatures.scss";
 import Home from "./Home";
 import About from "./About";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <main>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </main>
+    <>
+      <main>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
