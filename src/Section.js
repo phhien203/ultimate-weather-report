6cb9828c06a1b22ab910d7648ec44ad252272c7b
Pham Huu Hien <phhien203@gmail.com>
2022-05-24 22:54:35 +0700

    Update browser title by using react helmet

--- src/Section.js
+++ src/Section.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const Section = ({children, headingText, headingLevel = 1}) => {
+const Section = ({ children, headingText, headingLevel = 1 }) => {
   const H = `h${headingLevel}`;
-  
+
   return (
     <section>
+      <Helmet>
+        <title>{`${headingText} | Ultimate Weather Report`}</title>
+      </Helmet>
       <H>{headingText}</H>
       {children}
     </section>
